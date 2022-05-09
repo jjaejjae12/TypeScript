6e5ae88fe94ac4194745a6c8845ba8124967ce1a
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2022-05-09 22:50:07 +0900

    JSDoc
    JSDoc

--- blockchain/src/myPackage.js
+++ blockchain/src/myPackage.js
@@ -1,6 +1,21 @@
+
+//js에서 ts사용하기
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url       
+ * @returns {boolean}
+ */
 export function init(config) {
     return true;
 }
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns 
+ */
 export function exit(code) {
     return code + 1;
 }
